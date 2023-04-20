b66c7c3d1c5403f9caa3207e578e5e7160bd044c
Nouran Hossameldin <111601276+nouranhossamm@users.noreply.github.com>
2023-04-20 14:00:07 +0200

    Add files via upload

---
+++ Section_6.py
@@ -0,0 +1,71 @@
+# Huffman Code
+
+class Node:
+    def __init__(self, freq, symbol, left=None, right=None):
+        # frequency of symbol
+        self.freq = freq 
+        # symbol name 
+        self.symbol = symbol
+        # node left of current node 
+        self.left = left
+        # node right of current node
+        self.right = right 
+        # tree direction (0,1)
+        self.huff = ''
+        
+        
+def printNodes(node, val=''):
+    # huffman code for current node
+    newVal = val + str(node.huff)
+    # print(newVal)
+    
+    # if node is not an edge node
+    # then traverse inside it
+    if(node.left):
+        printNodes(node.left, newVal)
+    if(node.right):
+        printNodes(node.right, newVal)
+        
+    # if node is edge node
+    # then display its huffman code
+    if(not node.left and not node.right):
+        print(f"{node.symbol} → {newVal}")
+    
+
+# characters for Huffman tree
+chars = ['a', 'b', 'c', 'd', 'e', 'f']
+# frequency of  characters
+freq = [5, 9, 12, 13, 16, 45]
+# list containing unused nodes
+nodes = []
+# converting characters and frequencies
+# into Huffman tree nodes
+for x in range(len(chars)):
+    y = Node(freq[x], chars[x])
+    nodes.append(y)
+
+
+while len(nodes) > 1:
+    # sort all the nodes in ascending order 
+    # based on their frequency
+    nodes = sorted(nodes, key= lambda x:x.freq)
+    # pick 2 smallest nodes 
+    left = nodes[0]
+    right = nodes[1]
+    # assign directional value to these nodes
+    left.huff = 0
+    right.huff = 1
+    # combine the 2 smallest nodes to create
+    # new node as their parent
+    newNode = Node(left.freq + right.freq, left.symbol +right.symbol, left, right)
+    # remove the 2 nodes and add their 
+    # parent as new node among others
+    nodes.remove(left)
+    nodes.remove(right)
+    nodes.append(newNode)
+    
+# Huffman Tree is ready!
+printNodes(nodes[0])
+
+
+
